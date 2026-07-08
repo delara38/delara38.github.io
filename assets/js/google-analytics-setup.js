1c80c35df9fe9c3caef49a768e615019769d15f0
delara38 <48336104+delara38@users.noreply.github.com>
2026-07-08 23:23:55 +0000

    Deploying to gh-pages from @ delara38/delara38.github.io@52dafad8e41d423223d198fd22cdfb6d02419e5d 🚀

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -4,3 +4,13 @@ function gtag() {
 }
 gtag("js", new Date());
 gtag("config", "");
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-G31VTGHYKS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-G31VTGHYKS');
+</script>
